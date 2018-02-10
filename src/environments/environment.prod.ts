6940e692a2a2a54ae5920c8f852bed3126145a23
Amin Nagpure <aminnagpure@hotmail.com>
2018-02-10 09:22:53 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  config: {
+    firebase: {
+      apiKey: "AIzaSyCfqoCIUwLLfXxvEafa_Kj08LY6J1ptuMY",
+      authDomain: "fir-testing1-b9eed.firebaseapp.com",
+      databaseURL: "https://fir-testing1-b9eed.firebaseio.com",
+      projectId: "fir-testing1-b9eed",
+      storageBucket: "fir-testing1-b9eed.appspot.com",
+      messagingSenderId: "937085708414"
+    }
+  }
+};
